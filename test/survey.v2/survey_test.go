646deecb4844bd0e33a1bd9d137b64456c78a01b
Nathan Rijksen <n.rijksen@gmail.com>
2023-08-23 15:06:05 -0700

    Amend modifies commit, chicken and egg..

--- test/survey.v2/survey_test.go
+++ test/survey.v2/survey_test.go
@@ -26,7 +26,7 @@ func Test_Survey(t *testing.T) {
 	}
 
 	// We run this on a loop in order to surface potential race conditions between expect, send and the terminal emulator
-	// See commit 8b0580afe8c8e331a512fb600a068cbae9ead163
+	// See commit 90f0511212539829317b2652d57785c7a3cc71f8
 	for x := 0; x < 10; x++ {
 		tt.Expect(fmt.Sprintf("Confirm %d?", x))
 		tt.SendLine("y")
