6831d5fa1ad6f38fa61b7a995b0dc0b6a992627c
Nathan Rijksen <n.rijksen@gmail.com>
2023-09-08 15:48:24 -0700

    Fix compilation on mac/linux

--- helpers_unix.go
+++ helpers_unix.go
@@ -4,12 +4,11 @@
 package termtest
 
 import (
-	"bytes"
 	"errors"
 )
 
 var ERR_ACCESS_DENIED = errors.New("only used on windows, this should never match")
 
-func cleanPtySnapshot(b []byte, _ bool) []byte {
-	return bytes.TrimRight(b, "\x00")
+func cleanPtySnapshot(b []byte, cursorPos int, _ bool) ([]byte, int) {
+	return b, cursorPos
 }
