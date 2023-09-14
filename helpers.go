6ad7900e97fe1338254d7265c651f545679d4c1a
Nathan Rijksen <n.rijksen@gmail.com>
2023-09-14 09:15:32 -0700

    Drop unused

--- helpers.go
+++ helpers.go
@@ -63,35 +63,6 @@ func unwrapErrorMessage(err error) string {
 	return strings.Join(msg, " -> ")
 }
 
-// cleanPtySnapshotWindows removes virtual escape sequences from the given byte slice
-// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
-func cleanPtySnapshotWindows(b []byte) (o []byte) {
-	// All escape sequences appear to end on `A-Za-z@`
-	virtualEscapeSeqEndValues := []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@")
-	inEscapeSequence := false
-
-	return bytes.Map(func(r rune) rune {
-		switch {
-		// Detect start of sequence
-		case !inEscapeSequence && r == 27:
-			inEscapeSequence = true
-			return -1
-
-		// Detect end of sequence
-		case inEscapeSequence && bytes.ContainsRune(virtualEscapeSeqEndValues, r):
-			inEscapeSequence = false
-			return -1
-
-		// Anything between start and end of escape sequence should also be dropped
-		case inEscapeSequence:
-			return -1
-
-		default:
-			return r
-		}
-	}, b)
-}
-
 func reverse[S ~[]E, E any](s S) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
